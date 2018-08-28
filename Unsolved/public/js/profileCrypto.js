62542950ef0e22bec46cdc0fd995a5716deb0e2c
fysics <j.mills.ucf.cbc@gmail.com>
2018-08-28 16:56:37 -0400

    Modifications before start of class

--- Unsolved/public/js/profileCrypto.js
+++ Unsolved/public/js/profileCrypto.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-  var queryURL = "https://api.coingecko.com/api/v3/coins?order=market_cap_&per_page=15";
+    var queryURL = "https://api.coingecko.com/api/v3/coins?order=market_cap_&per_page=24";
 
     $.ajax({
         url: queryURL,
@@ -51,4 +51,49 @@ $(document).ready(function() {
             }
         })
 
+    $(document).on("click", ".crypto-btn", function (event) {
+        event.preventDefault();
+
+        console.log(this.id);
+        var queryURL = "https://api.coingecko.com/api/v3/coins/" + this.id;
+        console.log(queryURL);
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function (response) {
+                console.log(response);
+                var results = response;
+
+                var name = results.name;
+                var idG = results.id;
+                var symbol = results.symbol;
+                var image = results.image.small;
+                var currentPrice = Math.round(results.market_data.current_price.usd);
+                var hourChange = Math.round(results.market_data.price_change_percentage_24h);
+                var description = results.description.en;
+
+                var idDiv = $("<div>").text("ID: " + idG);
+                var symbolDiv = $("<div>").text("Ticker: " + symbol.toUpperCase());
+                var nameDiv = $("<div>").text("Name: " + name);
+                var currentPriceDiv = $("<div>").text("Current Price: $" + currentPrice + "USD");
+                var hourChangeDiv = $("<div>").text("Change 24H: " + hourChange + "%");
+                var imgDiv = $("<img>").attr("src", image);
+                var descDiv = $("<div>").text(description);
+
+                $("#dataDiv").html("");
+                $("#dataDiv").append(imgDiv);
+                $("#dataDiv").append(idDiv);
+                $("#dataDiv").append(symbolDiv);
+                $("#dataDiv").append(nameDiv);
+                $("#dataDiv").append(currentPriceDiv);
+                $("#dataDiv").append(hourChangeDiv);
+                $("#dataDiv").append(descDiv);
+
+            });
+
+    });
+
 });
+  
